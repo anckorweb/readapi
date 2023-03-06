af82160f4dd4ca0470c1a875e7301536cd1afb67
Ancor Santos Pérez <anckorweb@gmail.com>
2023-03-06 22:03:16 +0000

    ReadApi: Hook to read api with fetch

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useFetch } from "./useFetch";
+import "./App.css";
+
+function App() {
+  const { data, loading, error } = useFetch(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+
+  return (
+    <div className="App">
+      <h1>Fetch like a pro</h1>
+      <div className="card">
+        <ul>
+          {error && <li>Error: {error}</li>}
+          {loading && <li>Loading...</li>}
+          {data?.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
